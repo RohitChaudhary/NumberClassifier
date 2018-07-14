fb1f8db47f5eb2b7ead3e89f71fe576d5740153b
Rohit Chaudhary <Rohit.chry@gmail.com>
2018-07-14 16:45:55 -0700

    Added functionality to draw on canvas and clear

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,57 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var context;
+var canvas;
+var pressed;
+var oldX, oldY;
+
+function init() {
+  console.log("init");
+  canvas = document.getElementById('inputCanvas');
+  context = canvas.getContext("2d");
+
+  canvas.addEventListener("mousedown", function (element) {
+    pressed = true;
+    draw(element.pageX - canvas.offsetLeft, element.pageY - canvas.offsetTop,
+    false);
+  }, false);
+
+  canvas.addEventListener("mousemove", function (element) {
+    if (pressed) {
+      draw(element.pageX - canvas.offsetLeft, element.pageY - canvas.offsetTop,
+      true);
+    }
+  }, false);
+
+  canvas.addEventListener("mouseup", function (element) {
+    pressed = false;
+  }, false);
+
+  canvas.addEventListener("mouseleave", function (element) {
+    pressed = false;
+  }, false);
+
+  document.getElementById('clearButton').addEventListener("click", clear);
+}
+
+function draw(x, y, isPressed) {
+  if (isPressed) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = "8";
+    context.lineJoin = "round";
+    context.moveTo(oldX, oldY);
+    context.lineTo(x, y);
+    context.closePath();
+    context.stroke();
+  }
+  oldX = x;
+  oldY = y;
+}
+
+function clear() {
+  console.log("clearing board");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
